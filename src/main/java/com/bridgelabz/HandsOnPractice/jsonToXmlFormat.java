fadb9327591fe4dcf0b3758708ac6909d6cb1629
Yagyata <yagyatasharmaa@gmail.com>
2025-04-11 09:59:10 +0530

    [Yagyata] ADD: Convert JSON to XML format

---
+++ src/main/java/com/bridgelabz/HandsOnPractice/jsonToXmlFormat.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.HandsOnPractice;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class jsonToXmlFormat {
+    public static void main(String[] args) {
+        try {
+            String jsonFormat = "{\"name\":\"Yagyata\",\"age\":21}";
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode jsonNode = mapper.readTree(jsonFormat);
+
+            XmlMapper xmlMapper = new XmlMapper();
+            String xml = xmlMapper.writeValueAsString(jsonNode);
+
+            System.out.println(xml);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
